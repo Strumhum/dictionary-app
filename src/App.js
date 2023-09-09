9b62817dba69812b258d43640ec8bdc890b8d449
Strumhum <sue@strumhumcreatives.com>
2023-09-09 13:55:45 +0100

    Added basic Search form and function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello World</header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header text-center">Dictionary</header>
+        <Dictionary />
+        <footer className="App-footer text-center small">
+          Coded by Sue McMillan
+        </footer>
+      </div>
     </div>
   );
 }
