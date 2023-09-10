c5ee08b53463fc69bdf314dd3a84d47c8e724318
Strumhum <sue@strumhumcreatives.com>
2023-09-10 10:23:25 +0100

    Rendered meanings, descriptions and examples

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
 
+    // https://www.shecodes.io/learn/apis/dictionary documentation link
     let apiKey = `ef85b2a56a7f4e1tc1e01fdofd0bbc3b`;
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
@@ -25,8 +27,9 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" onChange={handleKeyword} />
+        <input type="search" name="search-field" onChange={handleKeyword} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
