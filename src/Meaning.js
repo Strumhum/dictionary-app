3e8e3a412177d569b9cedc28af3d9fa57e8a1874
Strumhum <sue@strumhumcreatives.com>
2023-09-11 01:53:16 +0100

    Integrated synonyms to search results

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,7 +1,7 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
@@ -10,6 +10,7 @@ export default function Meaning(props) {
         <br />
         <em>{props.meaning.example}</em>
       </p>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
