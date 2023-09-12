c2f50bc17c32297d0e94a8b390a316ec1172edc3
Strumhum <sue@strumhumcreatives.com>
2023-09-12 01:19:51 +0100

    Added Photos search component via keyword search

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+    console.log(response.data.photos);
   }
 
   function search() {
@@ -17,6 +24,12 @@ export default function Dictionary(props) {
     let apiKey = `ef85b2a56a7f4e1tc1e01fdofd0bbc3b`;
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+
+    const pexelsApiKey =
+      "zfL4diViCR3o1OJpeC97BqDxbOhZSNF9LLLBrLb5wZzed6MIGU4lV7Hu";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -47,6 +60,7 @@ export default function Dictionary(props) {
           </small>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
