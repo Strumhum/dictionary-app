9ea95f0aab49dc03ee301fa70a9b30bf4d0531b4
Strumhum <sue@strumhumcreatives.com>
2023-09-12 01:25:23 +0100

    Fixed Photos code for API call

--- src/Photos.js
+++ src/Photos.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Photos.css";
+import "./Dictionary.css";
 
 export default function Photos(props) {
   if (props.photos) {
@@ -10,7 +11,7 @@ export default function Photos(props) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src="{photo.src.landscape}" className="img-fluid" />
+                  <img src={photo.src.landscape} className="img-fluid" />
                 </a>
               </div>
             );
